9e919b1ab457c3c465a2ca41f6549bdf11c70da2
CamiDev <lamica.olivares@gmail.com>
2025-12-03 17:30:15 -0500

    jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/User');
+const User = require('../models/User');
+const { generarJWT } = require('../helpers/jwt');
 
 
 const newUsuario = async (req, res = response) => {
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
 
     try {
 
@@ -26,11 +28,14 @@ const newUsuario = async (req, res = response) => {
 
         await usuario.save();
 
+        const token = await generarJWT(usuario.id, usuario.name)
+
         res.status(201).json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
-           
+            name: usuario.name,
+            token
+
         })
     } catch (error) {
         res.status(500).json({
@@ -41,8 +46,45 @@ const newUsuario = async (req, res = response) => {
 
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
     const { email, password } = req.body;
+
+    try {
+
+        const usuario = await Usuario.findOne({ email });
+
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User no existe con esa convinacion :)'
+            });
+        }
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password Incorrecto'
+            })
+
+        }
+
+        const token = await generarJWT(usuario.id, usuario.name)
+
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: User.name,
+            token
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Escriba al Admin'
+        })
+    }
 
     res.status(201).json({
         ok: true,
