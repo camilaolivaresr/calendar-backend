8308447fbfcb049c751f2a6c3cafe48a06ab36b2
CamiDev <lamica.olivares@gmail.com>
2025-12-14 13:26:58 -0500

    udate event

--- controllers/events.js
+++ controllers/events.js
@@ -38,13 +38,55 @@ const createEvent = async (req, res = response) => {
 }
 
 
-const updateEvent = (req, res = response) => {
+const updateEvent = async(req, res = response) => {
 
+    const eventoId = req.params.id;
+    const uid = req.uid;
 
-    res.json({
-        ok: true,
-        msg: 'actualizar eventos'
-    })
+    try {
+
+        const evento = await Evento.findById( eventoId );
+    
+        
+        if( !evento ){
+            res.status(404).json({
+                ok: false,
+                msg: 'No existe evet por id'
+            });
+        }
+        
+        if( evento.user.toString() !== uid) {
+                return res.status(401).json({
+                ok: false,
+                msg: 'No privilegio'
+            });
+        }
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const updateNewEvent = await Evento.findByIdAndUpdate(eventoId, newEvent, {new: true});
+       
+        res.json({
+            ok: true,
+            evento: updateNewEvent
+        })
+        
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con la Admin'
+        });
+        
+    }
+
+    // res.json({
+    //     ok: true,
+    //     eventoId
+    // })
 }
 
 
